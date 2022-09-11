e8ec74ade53afbd2ec637048a99677559dc21700
Paul Krupski <pjkrupski@gmail.com>
2022-09-11 18:21:56 -0400

    bakery complete

--- app.js
+++ app.js
@@ -26,14 +26,26 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  //console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
+    //words.filter(word => word.length > 6);
+  const filtered = fruitsArray.filter(objects => { return objects.nutritions.sugar > 15});
+  console.log(filtered);
+
   // TODO: Create a new HTML element to display your data
+  const mapped = filtered.map(fruit => {
+    return fruit.name;   
+  });
+
+  
+  console.log(mapped);
+
 
   // TODO: Append your new element to the page
+  document.getElementById('return-info').append(mapped);
 
 }
 
